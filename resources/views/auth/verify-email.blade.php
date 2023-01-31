3a0342ffd90f2441202bfc1299dc9b03f38c602c
Gustavo Cuerva <cuervagustavo@gmail.com>
2023-01-31 17:59:27 -0300

    Email enviando email de verificação

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,45 +1,48 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+@extends('layouts.main')
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Before continuing, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
-        </div>
+@section('title', 'Confirme seu email')
 
+@section('styles')
+    <link rel="stylesheet" href="{{asset('css/login.css')}}">
+@endsection
+
+@section('class_body', 'body_login')
+
+@section('content')
+<section class="login">
+
+        <h2>Confirme seu email</h2>
         @if (session('status') == 'verification-link-sent')
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ __('A new verification link has been sent to the email address you provided in your profile settings.') }}
+            <div class="alert alert-success">
+                {{ __('Um novo link de verificação foi enviado para o endereço de e-mail que você forneceu nas configurações do seu perfil.') }}
             </div>
         @endif
+        <br>
+        <div class="mb-4 text-sm text-gray-600">
+            {{ __('Antes de continuar, você poderia verificar seu endereço de e-mail clicando no link que acabamos de enviar para você? Se você não recebeu o e-mail, teremos o prazer de lhe enviar outro.') }}
+        </div>
 
         <div class="mt-4 flex items-center justify-between">
             <form method="POST" action="{{ route('verification.send') }}">
                 @csrf
 
                 <div>
-                    <x-jet-button type="submit">
-                        {{ __('Resend Verification Email') }}
+                    <x-jet-button type="submit" style="cursor:pointer; padding: 10px; border: none; font-size: 15px; color: white; background: #ff9a00;">
+                        {{ __('Reenviar email de verificação') }}
                     </x-jet-button>
                 </div>
             </form>
 
             <div>
-                <a
-                    href="{{ route('profile.show') }}"
-                    class="underline text-sm text-gray-600 hover:text-gray-900"
-                >
-                    {{ __('Edit Profile') }}</a>
 
                 <form method="POST" action="{{ route('logout') }}" class="inline">
                     @csrf
 
-                    <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 ml-2">
-                        {{ __('Log Out') }}
+                    <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 ml-2" style="padding: 10px; border: none; font-size: 15px;">
+                        {{ __('Sair') }}
                     </button>
                 </form>
             </div>
         </div>
-    </x-jet-authentication-card>
-</x-guest-layout>
+</section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -27,17 +27,17 @@ Route::get('/', [ProductController::class, 'index'])->name('index');
 
 Route::prefix('/reserves')->group(function(){
     Route::get('/', [ReservationController::class, 'create'])->name('reserve');
-    Route::get('/reserves', [ReservationController::class, 'show'])->middleware('auth')->name('reserves');
-    Route::post('/reserve', [ReservationController::class, 'store'])->middleware('auth')->name('reserve.store');
-    Route::put('/confirm', [ReservationController::class, 'confirm'])->middleware('auth')->name('reserve.confirm');
-    Route::put('/cancel', [ReservationController::class, 'cancel'])->middleware('auth')->name('reserve.cancel');
+    Route::get('/reserves', [ReservationController::class, 'show'])->middleware('auth')->middleware('verified')->name('reserves');
+    Route::post('/reserve', [ReservationController::class, 'store'])->middleware('auth')->middleware('verified')->name('reserve.store');
+    Route::put('/confirm', [ReservationController::class, 'confirm'])->middleware('auth')->middleware('verified')->name('reserve.confirm');
+    Route::put('/cancel', [ReservationController::class, 'cancel'])->middleware('auth')->middleware('verified')->name('reserve.cancel');
 });
 
-Route::get('/products/favorites/{id}', [ProductController::class, 'favorites_list'])->middleware('auth')->name('favorites');
+Route::get('/products/favorites/{id}', [ProductController::class, 'favorites_list'])->middleware('auth')->middleware('verified')->name('favorites');
 Route::prefix('/products')->group(function(){
     Route::get('/menu/{id}', [ProductController::class, 'list'])->name('menu');
     Route::get('/{id}', [ProductController::class, 'show'])->name('product');
-    Route::post('/', [ProductController::class, 'saved'])->middleware('auth')->name('saved.product');
+    Route::post('/', [ProductController::class, 'saved'])->middleware('auth')->middleware('verified')->name('saved.product');
 });
 
 Route::get('/about', [InformationController::class, 'about'])->name('about');
@@ -45,37 +45,38 @@ Route::get('/about', [InformationController::class, 'about'])->name('about');
 Route::prefix('dashboard')->group(function(){
     
     // Category
-    Route::get('/category', [CategoryController::class, 'create'])->middleware('auth')->name('admin.category');
-    Route::get('/category/{id}', [CategoryController::class, 'show_edit'])->middleware('auth')->name('edit.category');
-    Route::post('/category', [CategoryController::class, 'store'])->middleware('auth')->name('store.category');
-    Route::put('/category', [CategoryController::class, 'update'])->middleware('auth')->name('store.category');
-    Route::delete('/category', [CategoryController::class, 'destroy'])->middleware('auth')->name('del.category');
+    Route::get('/category', [CategoryController::class, 'create'])->middleware('auth')->middleware('verified')->name('admin.category');
+    Route::get('/category/{id}', [CategoryController::class, 'show_edit'])->middleware('auth')->middleware('verified')->name('edit.category');
+    Route::post('/category', [CategoryController::class, 'store'])->middleware('auth')->middleware('verified')->name('store.category');
+    Route::put('/category', [CategoryController::class, 'update'])->middleware('auth')->middleware('verified')->name('store.category');
+    Route::delete('/category', [CategoryController::class, 'destroy'])->middleware('auth')->middleware('verified')->name('del.category');
 
     // Products
-    Route::get('/products/{id}', [ProductController::class, 'listProducts'])->middleware('auth')->name('admin.products');
-    Route::get('/products/create/{id}', [ProductController::class, 'create'])->middleware('auth')->name('create.products');
-    Route::get('/products/edit/{id}', [ProductController::class, 'edit'])->middleware('auth')->name('edit.products');
-    Route::post('/products/create', [ProductController::class, 'store'])->middleware('auth')->name('store.products');
-    Route::put('/products/update', [ProductController::class, 'update'])->middleware('auth')->name('update.products');
-    Route::delete('/products', [ProductController::class, 'destroy'])->middleware('auth')->name('delete.products');
+    Route::get('/products/{id}', [ProductController::class, 'listProducts'])->middleware('auth')->middleware('verified')->name('admin.products');
+    Route::get('/products/create/{id}', [ProductController::class, 'create'])->middleware('auth')->middleware('verified')->name('create.products');
+    Route::get('/products/edit/{id}', [ProductController::class, 'edit'])->middleware('auth')->middleware('verified')->name('edit.products');
+    Route::post('/products/create', [ProductController::class, 'store'])->middleware('auth')->middleware('verified')->name('store.products');
+    Route::put('/products/update', [ProductController::class, 'update'])->middleware('auth')->middleware('verified')->name('update.products');
+    Route::delete('/products', [ProductController::class, 'destroy'])->middleware('auth')->middleware('verified')->name('delete.products');
 
     // Information
-    Route::get('/information', [InformationController::class, 'edit'])->middleware('auth')->name('information');
-    Route::put('/information', [InformationController::class, 'update'])->middleware('auth')->name('information');
+    Route::get('/information', [InformationController::class, 'edit'])->middleware('auth')->middleware('verified')->name('information');
+    Route::put('/information', [InformationController::class, 'update'])->middleware('auth')->middleware('verified')->name('information');
 
     // Reserves
-    Route::get('/reserves/{id}', [ReservationController::class, 'showAdmin'])->middleware('auth')->name('reserves.admin');
+    Route::get('/reserves/{id}', [ReservationController::class, 'showAdmin'])->middleware('auth')->middleware('verified')->name('reserves.admin');
 });
 
-Route::post('/assessments', [AssessmentController::class, 'store'])->middleware('auth')->name('store.assessments');
-Route::put('/assessments', [AssessmentController::class, 'update'])->middleware('auth')->name('update.assessments');
+Route::post('/assessments', [AssessmentController::class, 'store'])->middleware('auth')->middleware('verified')->name('store.assessments');
+Route::put('/assessments', [AssessmentController::class, 'update'])->middleware('auth')->middleware('verified')->name('update.assessments');
 
-Route::get('/my-data', [UserController::class, 'index'])->middleware('auth')->name('my-data');
-Route::put('/my-data', [UserController::class, 'update'])->middleware('auth')->name('my-data.update');
-Route::delete('/my-data', [UserController::class, 'destroy'])->middleware('auth')->name('delete.user');
+Route::get('/my-data', [UserController::class, 'index'])->middleware('auth')->middleware('verified')->name('my-data');
+Route::put('/my-data', [UserController::class, 'update'])->middleware('auth')->middleware('verified')->name('my-data.update');
+Route::delete('/my-data', [UserController::class, 'destroy'])->middleware('auth')->middleware('verified')->name('delete.user');
 
-Route::get('/end', [UserController::class, 'close'])->middleware('auth');
+Route::get('/end', [UserController::class, 'close'])->middleware('auth')->middleware('verified');
 
+// Envio de email
 Route::get('/send-mail', [MailController::class, 'sendMail'])->name('send.mail');
 
 Route::fallback(function(){
@@ -87,5 +88,5 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth')->name('dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth')->middleware('verified')->name('dashboard');
 });
